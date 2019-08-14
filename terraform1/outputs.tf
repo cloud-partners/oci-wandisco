e4e5511636c023897f644f7e78c9f862f3fe4865
Oguz Pastirmaci <oguzp@outlook.com>
2019-08-14 11:36:07 -0700

    Fix typo in output

--- terraform1/outputs.tf
+++ terraform1/outputs.tf
@@ -1,6 +1,6 @@
 output "Your server has this IP" {
   value = ["${data.oci_core_vnic.fusion_server_vnic.public_ip_address}",
-    "In about 5 mintues, browse and login with this FusionServerURL: http://${data.oci_core_vnic.fusion_server_vnic.public_ip_address}:8083 ",
+    "In about 5 minutes, browse and login with this FusionServerURL: http://${data.oci_core_vnic.fusion_server_vnic.public_ip_address}:8083 ",
     "    with username:  ${var.fusion_server["adminUsername"]} ",
     "     and password:  ${random_string.password.result}",
   ]
