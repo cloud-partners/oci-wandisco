bfc061094daedb880e5a21663afda1718f1251ac
Collin Poczatek <collin.poczatek@gmail.com>
2020-02-10 14:07:15 -0800

    remove hardcoded testing values, rotate keys

--- variables.tf
+++ variables.tf
@@ -20,16 +20,11 @@ variable "private_key_path" {}
 variable "ssh_public_key" {}
 
 # Object Storage
-variable "bucket" {
-}
+variable "bucket" {}
 
-variable "accesskey" {
-  default = "cdd7cefb47c573d847d8cecba75d96658ede6312"
-}
+variable "accesskey" {}
 
-variable "secretkey" {
-  default = "QVM2lZ7O8WNa6V8iG/PP4jOVqi3pjlE+iH3M/Sumots="
-}
+variable "secretkey" {}
 
 variable "zone" {
   default = "Proxy1"
@@ -47,17 +42,9 @@ variable "adminUsername" {
   default = "admin"
 }
 
-variable "adminPassword" {
-  default = "admin"
-}
+variable "adminPassword" {}
 
-variable "node_count" {
-  default = 1
-}
-
-variable "base64_key" {
-  default = "Rxk49P0DE6859kmQVrttpavRcgrUllgZhinnqxO+IpjC7ZPRv6DM7PnG4Qh6Nyg5FCkwoSg/NoTBu1iB8XK9kO8MWWgVDOfjIi4wQC6w6ScIrTUmLrXS+Qlx3yJK98MwHSj5NjxQGD7HJPJylDeTy98oV5dY2aqmp5H1xc0nCFGErXKJWe9YaEsjmibCniMlTDl5amb2AH4tWWyROPHJnZ1Gr/1R7uVZN1KgavcdgWk7nBHv5iCzXl/pJzsYs1xwelU7kF0Hl9LuPfRbKg6M8ErmkbbKaKrYHWcarR8od1NCWy/h1805MuOYAcY8FL/2B5atZpnwjPWriR+mjSUbzB6swRCYSyUAxDWtLn6K8p2zxKvyV98Oh9+H+urAjGLtFWoIS6zMPFPVAaoSp3d8ys8WkX7PThY3aAMfzVOoHqCHOWFkczuQKKSOvxK/3KiBFLIxTiQ2ZzDmrkXrdNMC0NHN1lHMcUTC2KK5Xa5fNfJD2Dju26GRKKHi0ocRGGS6jnbod6GxpArdamoFuSeFV/Pt/R5j2YlDtPzYAs3Kyuf/qG6LjydgsXlPDXX0BsuvKmojNDmw1j3YNClQaf3tlZbr8UwG1xMQ2tQad+lDr3/g7+n4NIy9zkkv3Ob7g7Ca3blJup9I4GR/gIr+UYE+VxwKfCyfYf28z8Q2RlSDmec="
-}
+variable "base64_key" {}
 
 variable "proxy" {
   default = "0.0.0.0:8071"
